66b3d18648982f15ee7a2208d737f6190a469a3f
FrancoAquino23 <francoaquialla@gmail.com>
2026-07-08 12:55:45 -0600

    Feat (Frontend): Wire tutorial modal

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -6,20 +6,32 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { phosphorRobot } from '@ng-icons/phosphor-icons/regular';
+import { phosphorQuestionBold } from '@ng-icons/phosphor-icons/bold';
 import { AuthService } from './core/auth.service';
 import { PlayerStateService } from './core/player-state.service';
+import { UiStateService } from './core/ui-state.service';
 import { ToastComponent } from './shared/toast/toast.component';
 import { ProfileModalComponent } from './shared/profile-modal/profile-modal.component';
+import { TutorialModalComponent } from './shared/tutorial-modal/tutorial-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ToastComponent, ProfileModalComponent, NgIconComponent],
-  viewProviders: [provideIcons({ phosphorRobot })],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ToastComponent,
+    ProfileModalComponent,
+    TutorialModalComponent,
+    NgIconComponent,
+  ],
+  viewProviders: [provideIcons({ phosphorRobot, phosphorQuestionBold })],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   auth = inject(AuthService);
   playerState = inject(PlayerStateService);
-  profileOpen = signal(false);
+  uiState = inject(UiStateService);
+  tutorialOpen = signal(false);
 }
